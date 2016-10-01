540f37d2cf5f6b0d60441fdba81f31cb7043435b
stokor <gl123098@gmail.com>
2016-10-01 11:24:49 -0300

    Se agrega mensaje de bienvenida

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
+void mostrarMensajeBienvenida()
+{
+        printf("Bienvenido al sistema.");
+}
+
+
 void main()
 {
         printf("Hola Carola");
+        mostrarMensajeBienvenida();
 }
